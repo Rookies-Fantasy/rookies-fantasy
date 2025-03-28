4de03d48f880bd64a1d39bfdab70310b7576f1f1
NasihNazeem <nasihnazeem@gmail.com>
2025-03-28 13:50:09 -0600

    integrated gluestack provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,11 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/components/useColorScheme";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -32,11 +28,11 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode="light">
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
-    </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
