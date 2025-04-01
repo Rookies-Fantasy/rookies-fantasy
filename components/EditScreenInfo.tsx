61fe4dee6096f25e976a12b9705bb894e7ea67b3
NasihNazeem <nasihnazeem@gmail.com>
2025-04-01 15:20:17 -0600

    cleanup of unused template files

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,8 +1,5 @@
 import { View, Text } from "react-native";
 
-import { ExternalLink } from "./ExternalLink";
-import { MonoText } from "./StyledText";
-
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
@@ -12,7 +9,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
         </Text>
 
         <View className="rounded-xl px-4 my-7">
-          <MonoText>{path}</MonoText>
+          <Text className="text-pbk-b1">{path}</Text>
         </View>
 
         <Text className="text-pbk-bl leading-6 text-center">
@@ -22,15 +19,9 @@ export default function EditScreenInfo({ path }: { path: string }) {
       </View>
 
       <View className="mt-15 mx-20 items-center">
-        <ExternalLink
-          className="py-15"
-          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
-        >
-          <Text className="text-center">
-            Tap here if your app doesn't automatically update after making
-            changes
-          </Text>
-        </ExternalLink>
+        <Text className="text-center">
+          Tap here if your app doesn't automatically update after making changes
+        </Text>
       </View>
     </View>
   );
