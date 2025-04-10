64d411b9763bce734d8a6c478ecaf98c24b66d15
NasihNazeem <nasihnazeem@gmail.com>
2025-04-10 12:56:16 -0600

    ENG-6: removed unnecessary string literal on class names

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function TabTwoScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-base-white">
-      <Text className={`pbk-h6 text-gray-950`}>Tab Two</Text>
+      <Text className="pbk-h6 text-gray-950">Tab Two</Text>
       <View className="my-30 h-16" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -6,12 +6,10 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
       <View className="flex-1 items-center justify-center p-5">
-        <Text className={`pbk-h1 text-gray-950`}>
-          This screen doesn't exist.
-        </Text>
+        <Text className="pbk-h1 text-gray-950">This screen doesn't exist.</Text>
 
         <Link href="/" className="mt-4 py-4">
-          <Text className={`pbk-b2 text-purple-500`}>Go to home screen!</Text>
+          <Text className="pbk-b2 text-purple-500">Go to home screen!</Text>
         </Link>
       </View>
     </>
