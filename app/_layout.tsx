fe2db0dc6094e394ba4e05cc444e526a47c53480
NasihNazeem <nasihnazeem@gmail.com>
2025-04-11 20:46:30 -0600

    ENG-6: Added a statusbar to root of app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
-
+import { StatusBar } from "expo-status-bar";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 import { useFonts } from "expo-font";
@@ -33,6 +33,7 @@ function RootLayoutNav() {
   return (
     <GluestackUIProvider>
       <GestureHandlerRootView>
+        <StatusBar />
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
