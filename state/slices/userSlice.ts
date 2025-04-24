2b7480c1acbd0e7a46942ea2c577acfd6f448210
NasihNazeem <nasihnazeem@gmail.com>
2025-04-24 12:20:39 -0600

    ENG-13: introduced a clear user reducer

--- state/slices/userSlice.ts
+++ state/slices/userSlice.ts
@@ -1,7 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
-type CurrentUser = {
+export type CurrentUser = {
   isLoading: boolean;
   userId?: string;
   username?: string;
@@ -19,22 +19,26 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setDisplayName: (state, action: PayloadAction<string>) => {
-      state.displayName = action.payload;
-    },
     setUser: (_, action: PayloadAction<CurrentUser>) => {
       return {
         ...action.payload,
       };
     },
+    setDisplayName: (state, action: PayloadAction<string>) => {
+      state.displayName = action.payload;
+    },
     setLoading: (state, action: PayloadAction<boolean>) => {
       state.isLoading = action.payload;
+    },
+    clearUser: () => {
+      return { isLoading: true };
     },
   },
 });
 
 export const selectCurrentUserId = (state: RootState) => state.user.userId;
 
-export const { setDisplayName, setUser, setLoading } = userSlice.actions;
+export const { setDisplayName, setUser, setLoading, clearUser } =
+  userSlice.actions;
 
 export default userSlice.reducer;
