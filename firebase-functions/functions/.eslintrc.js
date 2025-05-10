b76325d0a5f3b104534bc9e1b6d351292d16c008
NasihNazeem <nasihnazeem@gmail.com>
2025-05-10 14:15:08 -0600

    ENG-13: remove __dirname from eslintrc

--- firebase-functions/functions/.eslintrc.js
+++ firebase-functions/functions/.eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["./tsconfig.json", "tsconfig.dev.json"],
-    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
