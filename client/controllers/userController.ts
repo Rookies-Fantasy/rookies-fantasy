7d00d929a634fb979a29f5a2cb8f1491d0cf5efb
jay_chuang <jay.chuang@qubeiot.com>
2025-05-14 22:37:21 -0600

    eng-15: Add userController and endpoints

---
+++ client/controllers/userController.ts
@@ -0,0 +1,26 @@
+﻿import firestore from "@react-native-firebase/firestore";
+
+export type UserEditModel = {
+  name: string;
+  username: string;
+  dateOfBirth: Date;
+};
+
+export class UserController {
+  static getUser = async (userId: string) => {
+    try {
+      const userDoc = await firestore().collection("users").doc(userId).get();
+      return userDoc.exists ? userDoc.data() : undefined;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  static editUser = async (userId: string, params: UserEditModel) => {
+    try {
+      await firestore().collection("users").doc(userId).update(params);
+    } catch (error) {
+      throw error;
+    }
+  };
+}
