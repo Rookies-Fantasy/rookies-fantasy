9dd9939c9bbf25b9a5c0cafda2ae9487b180da27
NasihNazeem <nasihnazeem@gmail.com>
2025-05-23 10:21:01 -0600

    ENG-13: change to pressable to avoid href

--- client/app/(auth)/createProfile.tsx
+++ client/app/(auth)/createProfile.tsx
@@ -1,5 +1,4 @@
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useAppDispatch, useAppSelector } from "@/state/hooks";
 import { setUsername } from "@/state/slices/userSlice";
 
@@ -13,13 +12,12 @@ const CreateProfile = () => {
       <Text className="pbk-b1 text-gray-950">{user.email}</Text>
       <Text className="pbk-b1 text-gray-950">{user.username}</Text>
 
-      <Link
+      <Pressable
         className="mt-4 py-4"
-        href="/(protected)"
         onPress={() => dispatch(setUsername("testing"))}
       >
         <Text className="pbk-b2 text-purple-500">Go to home screen!</Text>
-      </Link>
+      </Pressable>
     </View>
   );
 };
