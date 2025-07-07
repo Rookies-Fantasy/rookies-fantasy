2954a75ada0abdd40f625c3314aa4b2d8431ad71
NasihNazeem <nasihnazeem@gmail.com>
2025-07-07 16:52:36 -0600

    ENG-20: Changed salary scale to round to hundred thousands, and made the salary calculation more clear

--- scripts/fillSalaries.js
+++ scripts/fillSalaries.js
@@ -27,30 +27,31 @@ const getSalaries = async () => {
 
 function linearScale(actualFpts, minFpts, maxFpts, minSalary, maxSalary) {
   return (
-    minSalary +
-    ((actualFpts - minFpts) * (maxSalary - minSalary)) / (maxFpts - minFpts)
+    (minSalary +
+      ((actualFpts - minFpts) * (maxSalary - minSalary)) /
+        (maxFpts - minFpts)) *
+    1000000
   );
+}
+
+function roundToHundredThousand(value) {
+  return Math.round(value / 100000) * 100000;
 }
 
 // EX: Nikola Jovic 22.6 FPTS
 // linScale func
-// salary = 10 000 000 + ((22.6 - 15) * (15 000 000 - 10 000 000)) / (25 - 15)
-// salary = 13 800 000
+// salary = (10 + ((22.6 - 15) * (15 - 10)) / (25 - 15) * 1000000)
+// salary = 13 820 000
 
 function calculateSalary(fpts) {
-  if (fpts === undefined || fpts === null || isNaN(fpts) || fpts < 0)
-    return 1000000;
+  if (fpts === undefined || fpts === null || isNaN(fpts) || fpts < 0) return 1;
 
-  if (fpts >= 50) return 35000000 + (fpts - 50) * 0.67;
-  else if (fpts >= 35 && fpts < 50)
-    return linearScale(fpts, 35, 50, 25000000, 35000000);
-  else if (fpts >= 25 && fpts < 35)
-    return linearScale(fpts, 25, 35, 15000000, 25000000);
-  else if (fpts >= 15 && fpts < 25)
-    return linearScale(fpts, 15, 25, 10000000, 15000000);
-  else if (fpts >= 5 && fpts < 15)
-    return linearScale(fpts, 5, 15, 5000000, 10000000);
-  else return linearScale(fpts, 0, 5, 1000000, 5000000);
+  if (fpts >= 50) return (35 + (fpts - 50) * 0.67) * 1000000;
+  else if (fpts >= 35 && fpts < 50) return linearScale(fpts, 35, 50, 25, 35);
+  else if (fpts >= 25 && fpts < 35) return linearScale(fpts, 25, 35, 15, 25);
+  else if (fpts >= 15 && fpts < 25) return linearScale(fpts, 15, 25, 10, 15);
+  else if (fpts >= 5 && fpts < 15) return linearScale(fpts, 5, 15, 5, 10);
+  else return linearScale(fpts, 0, 5, 1, 5);
 }
 
 const updateSalaries = async (players) => {
@@ -63,8 +64,9 @@ const updateSalaries = async (players) => {
 
     chunk.forEach((player) => {
       const salary = calculateSalary(player.fantasyPoints);
+      const roundedSalary = roundToHundredThousand(salary);
       batch.update(player.docRef, {
-        salary: salary,
+        salary: roundedSalary,
       });
     });
 
