ae5a431f054eecaccd8957a3a5f1d6c318a9cb04
NasihNazeem <nasihnazeem@gmail.com>
2025-07-25 16:24:38 -0600

    ENG-32: layout of roster page updated

--- client/app/(protected)/(draft)/roster.tsx
+++ client/app/(protected)/(draft)/roster.tsx
@@ -1,10 +1,52 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { ArrowLeft, X } from "phosphor-react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import TeamBudget from "@/components/TeamBudget";
 
-const Roster = () => (
-  <View className="flex-1 flex-col items-center justify-center bg-base-white px-6 py-4">
-    <Text className="pbk-h6 text-gray-950">Roster</Text>
-    <View className="my-30 h-16" />
-  </View>
-);
+const Roster = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView
+      className="flex-1 bg-gray-950"
+      edges={["top", "right", "left"]}
+    >
+      <Pressable className="flex-1" onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView className="flex-1">
+          <View className="px-6">
+            <View className="mb-10 flex-row items-center justify-between">
+              <View className="flex-row items-center gap-2">
+                <Pressable
+                  className="size-10 items-center justify-center rounded-md border border-gray-900 p-4"
+                  onPress={() => router.back()}
+                >
+                  <ArrowLeft color="white" size={20} weight="bold" />
+                </Pressable>
+                <Text className="pbk-h5 text-base-white">Team builder</Text>
+              </View>
+              <Pressable
+                className="size-10 items-center justify-center rounded-md border border-gray-900 p-4"
+                onPress={() => router.dismissAll()}
+              >
+                <X color="white" size={20} weight="bold" />
+              </Pressable>
+            </View>
+
+            <TeamBudget />
+          </View>
+          <Text className="pbk-h6 text-base-white">Roster</Text>
+          <View className="my-30 h-16" />
+        </KeyboardAvoidingView>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
 
 export default Roster;
