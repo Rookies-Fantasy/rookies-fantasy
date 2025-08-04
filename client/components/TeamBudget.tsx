629edd34fb296d82e5f8b7a3f6348ab3479005a3
NasihNazeem <nasihnazeem@gmail.com>
2025-08-04 14:32:25 -0600

    ENG-32: fixed balance negative symbol

--- client/components/TeamBudget.tsx
+++ client/components/TeamBudget.tsx
@@ -8,6 +8,9 @@ type TeamBudgetProps = {
 const TeamBudget = ({ className }: TeamBudgetProps) => {
   const teamBalance = useAppSelector((state) => state.team.balance);
 
+  const isNegative = teamBalance < 0;
+  const absBalance = Math.abs(teamBalance).toLocaleString();
+
   return (
     <View
       className={`w-full gap-1 rounded-lg border border-gray-800 bg-gray-900 p-4 ${className}`}
@@ -16,8 +19,11 @@ const TeamBudget = ({ className }: TeamBudgetProps) => {
         Available Balance
       </Text>
       <Text className="pbk-bl self-center text-base-white">
-        <Text className="pbk-bl text-green-600">
-          ${teamBalance.toLocaleString()}
+        <Text
+          className={`pbk-bl ${isNegative ? "text-red-600" : "text-green-600"}`}
+        >
+          {isNegative ? "-$" : "$"}
+          {absBalance}
         </Text>
         {` / $150,000,000`}
       </Text>
