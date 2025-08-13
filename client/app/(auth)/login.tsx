3da122e3abf8ddbc86b3f5bceba97097a5d45fe0
NasihNazeem <nasihnazeem@gmail.com>
2025-08-12 18:53:40 -0600

    ENG-32: Fix text spacing by using string literals

--- client/app/(auth)/login.tsx
+++ client/app/(auth)/login.tsx
@@ -221,12 +221,12 @@ const Login = () => {
           <SSOButtons />
 
           <Text className="pbk-b1 my-2 text-center text-gray-600">
-            Don&apos;t have an account?{" "}
+            Don&apos;t have an account?
             <Text
               className="text-purple-600"
               onPress={() => router.push("/(auth)/signUp")}
             >
-              Sign up
+              {` Sign up`}
             </Text>
           </Text>
         </KeyboardAvoidingView>
