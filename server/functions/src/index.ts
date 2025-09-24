9ff4656873351d8f9df30842b0cc285240be3b4b
NasihNazeem <nasihnazeem@gmail.com>
2025-09-23 23:42:18 -0600

    ENG-48: Remove unnecessary validation on server

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,9 +1,6 @@
 import type { UserRecord } from "firebase-admin/auth";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions/v1";
-import { onDocumentWritten } from "firebase-functions/firestore";
-import { validatePrerequisite } from "./augmentsValidation/validation";
-import { Player, Prerequisite } from "../types/augments";
 
 admin.initializeApp();
 
@@ -30,52 +27,3 @@ export const createUserInDatabase = functions.auth
       console.error("Error creating user in Firestore:", error);
     }
   });
-
-export const validateTeamAugment = onDocumentWritten(
-  "teams/{teamId}",
-  async (event) => {
-    const beforeData = event.data?.before.data();
-    const afterData = event.data?.after.data();
-
-    const lineupChanged =
-      JSON.stringify(beforeData?.lineup) !== JSON.stringify(afterData?.lineup);
-    const augmentChanged = beforeData?.augmentId !== afterData?.augmentId;
-
-    if (!lineupChanged && !augmentChanged) {
-      return;
-    }
-
-    const augmentDoc = await admin
-      .firestore()
-      .collection("augments")
-      .doc(afterData?.augmentId)
-      .get();
-
-    if (!augmentDoc.exists) return;
-
-    const augment = augmentDoc.data();
-    const result = validateAugmentPrerequisites(
-      augment?.prerequisites,
-      afterData?.lineup,
-    );
-  },
-);
-
-function validateAugmentPrerequisites(
-  prerequisites: Prerequisite[],
-  lineup: Player[],
-) {
-  const players = lineup; // Array of player objects
-  const qualifyingPlayers = new Set();
-  const unmetRequirements = [];
-
-  for (const prerequisite of prerequisites) {
-    const result = validatePrerequisite(prerequisite, players);
-
-    if (!result) {
-      unmetRequirements.push(prerequisite.description);
-    }
-  }
-
-  return unmetRequirements.length === 0;
-}
