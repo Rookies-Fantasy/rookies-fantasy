8aceead65912d32f90e56ecd19421d7044239b12
JayChuang8 <jaychuang1@gmail.com>
2025-12-24 22:40:36 -0700

    ENG-91: Fix type issue

--- client/components/MatchupInfoCard.tsx
+++ client/components/MatchupInfoCard.tsx
@@ -10,7 +10,7 @@ const EARLIEST_GAME_START_TIME_URL =
   "https://us-central1-rookies-fantasy-development.cloudfunctions.net/getEarliestGameStartTime";
 
 const MatchupInfoCard = () => {
-  const queueStatus: QueueStatus = useAppSelector(
+  const queueStatus: QueueStatus | undefined = useAppSelector(
     (state) => state.user.queueStatus,
   );
   const today = new Date();
