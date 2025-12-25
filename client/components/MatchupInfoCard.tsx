a771d8eda186194c94c41676c9957846a001793c
JayChuang8 <jaychuang1@gmail.com>
2025-12-24 21:59:58 -0700

    ENG-91: Add logic to call cloud function to get earliest game start time

--- client/components/MatchupInfoCard.tsx
+++ client/components/MatchupInfoCard.tsx
@@ -1,8 +1,14 @@
+import { getAuth } from "@react-native-firebase/auth";
 import { CheckCircle, HourglassHigh } from "phosphor-react-native";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { useCountdown } from "@/hooks/useCountdown";
 import { useAppSelector } from "@/state/hooks";
 import { selectUserQueueStatus } from "@/state/slices/userSlice";
 import { QueueStatus } from "@/types/user";
+
+const EARLIEST_GAME_START_TIME_URL =
+  "https://us-central1-rookies-fantasy-development.cloudfunctions.net/getEarliestGameStartTime";
 
 const MatchupInfoCard = () => {
   const queueStatus = useAppSelector(selectUserQueueStatus);
@@ -12,6 +18,50 @@ const MatchupInfoCard = () => {
     day: "2-digit",
     year: "numeric",
   });
+
+  const apiDate = today.toISOString().split("T")[0]; // YYYY-MM-DD
+
+  const [earliestStartTime, setEarliestStartTime] = useState<string>();
+
+  useEffect(() => {
+    if (queueStatus === QueueStatus.Queued) return;
+
+    const fetchEarliestGameTime = async () => {
+      try {
+        const auth = getAuth();
+        const currentUser = auth.currentUser;
+        if (!currentUser) {
+          console.error("No authenticated user");
+          return;
+        }
+
+        const idToken = await currentUser.getIdToken();
+
+        const response = await fetch(
+          `${EARLIEST_GAME_START_TIME_URL}?date=2025-12-25`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${idToken}`,
+            },
+          },
+        );
+
+        const data = await response.json();
+        console.log("DATA", data);
+
+        setEarliestStartTime(data.earliestGameStart);
+      } catch (error) {
+        console.log("Failed to fetch earliest game start time:", error);
+      }
+    };
+
+    fetchEarliestGameTime();
+  }, [apiDate, queueStatus]);
+
+  const countdown = useCountdown(
+    queueStatus !== QueueStatus.Queued ? earliestStartTime : null,
+  );
 
   return (
     <View className="w-full rounded-2xl bg-gray-900 p-4">
@@ -33,7 +83,13 @@ const MatchupInfoCard = () => {
           </View>
         ) : (
           <View className="flex-1">
-            <Text className="pbk-b2 text-base-white">Lineup locks</Text>
+            {countdown && (
+              <Text className="pbk-b2 text-base-white">
+                Lineup locks in{" "}
+                {countdown.hours > 0 ? `${countdown.hours}h ` : ""}
+                {countdown.minutes}m
+              </Text>
+            )}
 
             <Text className="pbk-b3 text-base-white">
               {`Your lineup locks for Today (${formattedDate})'s games once they start. Set your team before then.`}
