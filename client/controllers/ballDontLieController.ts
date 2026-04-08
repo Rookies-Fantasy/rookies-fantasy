fec66b4ff16dd4ef8f615f998a37c7661f618e41
NasihNazeem <nasihnazeem@gmail.com>
2026-04-08 12:36:25 -0600

    ENG-120: route cloud functions requests through emulator when using it

--- client/controllers/ballDontLieController.ts
+++ client/controllers/ballDontLieController.ts
@@ -1,10 +1,19 @@
 import { getAuth } from "@react-native-firebase/auth";
+import { Platform } from "react-native";
 import { isNotNil } from "@/utils/jsUtils";
 
-const EARLIEST_GAME_START_TIME_URL =
-  "https://us-central1-rookies-fantasy-development.cloudfunctions.net/getEarliestGameStartTime";
-const LIVE_DATA_URL =
-  "https://us-central1-rookies-fantasy-development.cloudfunctions.net/getLiveData";
+const FUNCTIONS_BASE_URL =
+  process.env.EXPO_PUBLIC_USE_EMULATOR === "true"
+    ? (() => {
+        const defaultHost =
+          Platform.OS === "android" ? "10.0.2.2" : "localhost";
+        const host = process.env.EXPO_PUBLIC_EMULATOR_HOST ?? defaultHost;
+        return `http://${host}:5001/rookies-fantasy-development/us-central1`;
+      })()
+    : "https://us-central1-rookies-fantasy-development.cloudfunctions.net";
+
+const EARLIEST_GAME_START_TIME_URL = `${FUNCTIONS_BASE_URL}/getEarliestGameStartTime`;
+const LIVE_DATA_URL = `${FUNCTIONS_BASE_URL}/getLiveData`;
 
 export const fetchEarliestGameStartTime = async (date: string) => {
   try {
